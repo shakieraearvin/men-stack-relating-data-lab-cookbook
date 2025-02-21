6f99fe200388dd3f0da4fd763782a9d9c4cf06da
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-21 15:25:12 -0500

    added delete route

--- controllers/foods.js
+++ controllers/foods.js
@@ -9,15 +9,13 @@ const User = require('../models/user.js');
 //
 router.get('/', async (req, res) => {
   try {
-    // Look up the user from req.session
+    
     const currentUser = await User.findById(req.session.user._id);
-    // Render index.ejs, passing in all of the current user's
-    // applications as data in the context object.
+
     res.render('foods/index.ejs', {
       foods: currentUser.foods,
     });
   } catch (error) {
-    // If any errors, log them and redirect back home
     console.log(error, 'There are no items in this pantry.');
     res.redirect('/');
   }
@@ -31,17 +29,12 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    // Look up the user from req.session
+  
     const currentUser = await User.findById(req.session.user._id);
-    // Push req.body (the new form data object) to the
-    // applications array of the current user
     currentUser.foods.push(req.body);
-    // Save changes to the user
     await currentUser.save();
-    // Redirect back to the applications index view
     res.redirect(`/users/${currentUser._id}/foods`);
   } catch (error) {
-    // If any errors, log them and redirect back home
     console.log(error);
     res.redirect('/');
   }
@@ -57,6 +50,25 @@ router.get('/:foodId', async (req, res) => {
     res.render('foods/show.ejs', {
       food: food,
     });
+  } catch (error) {
+
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// controllers/applications.js
+
+router.delete('/:foodId', async (req, res) => {
+  try {
+    
+    const currentUser = await User.findById(req.session.user._id);
+
+    currentUser.foods.id(req.params.foodId).deleteOne();
+   
+    await currentUser.save();
+ 
+    res.redirect(`/users/${currentUser._id}/foods`);
   } catch (error) {
 
     console.log(error);
